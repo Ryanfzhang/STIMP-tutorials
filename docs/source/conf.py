7ae060cd0468634c33e7ad5ad1931d0f4019e317
Ryanfzhang <mafzhang@ust.hk>
2025-02-28 11:38:29 +0800

    css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,11 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+html_static_path = ["_static"]
+
+def setup(app):
+    app.add_css_file("my_theme.css")
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
